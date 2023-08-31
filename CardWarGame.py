3d940aa9c74d0021e475989079cf782785f2cf58
Sajeesh V <sajeesh.v.tblr@gmai.com>
2023-08-31 18:26:59 +0530

    test commit

--- CardWarGame.py
+++ CardWarGame.py
@@ -4,6 +4,7 @@ ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Qu
 values={'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':11,'Queen':12,'King':13,'Ace':14}
 no_of_players=2
 
+
 class Card:
     def __init__(self,suit,rank):
         self.suit=suit
